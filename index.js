22b6389b52c7b52674310fdaa4d3f17677343a50
Darshan Santosh Borse <borsedarshan77@gmail.com>
2022-02-28 09:55:17 +0530

    Complete project

--- index.js
+++ index.js
@@ -3,12 +3,29 @@ import routes from './src/routes/crmRoutes';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
+import RateLimit from 'express-rate-limit';
+import cookieSession from 'cookie-session';
 
 const app = express();
 const PORT = 4000;
 
 // HTTP protection & Helmet setup 
 app.use(helmet());
+
+// Rate limit setup 
+//  Set time of IP request
+const limiter = new RateLimit({
+    windowMs: 15 * 60 * 1000,  // 15 minutes
+    max: 100,  // Limit of number of request per IP
+    delayMs: 0  // Disable dilays 
+});
+
+// Cookie Setup 
+app.use(cookieSession({
+    name: "darshan borse",
+    keys: "Darshan@123",
+    maxAge: 24 * 60 * 60 * 1000 //24 hour
+}));
 
 // mongoose connection
 mongoose.Promise = global.Promise;
